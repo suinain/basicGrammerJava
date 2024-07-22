a5acda2fcc1c87efe9697d04c74e73b559a4bd1b
suinain <suin961205@naver.com>
2024-07-22 13:31:43 +0900

    열혈강의 1강 완료

---
+++ selfCoding2/src/pracPPT/ch1/Page18.java
@@ -0,0 +1,8 @@
+package pracPPT.ch1;
+
+public class Page18 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+		System.out.println("First Java Program");
+	}
+}
